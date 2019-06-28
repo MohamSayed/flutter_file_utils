ae8dd0df8d4265ac46e53cb0c2b0de732d504b57
Mohamed Elsayed <mohamedelsayed6789@yahoo.com>
2019-06-29 01:48:11 +0200

    New search with filter example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,54 +1,86 @@
-// dart files
+// framework
 import 'dart:async';
 
-// framework
 import 'package:flutter/material.dart';
 
 // packages
-import 'package:path_provider/path_provider.dart';
 import 'package:flutter_file_manager/flutter_file_manager.dart';
-import 'package:simple_permissions/simple_permissions.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+@immutable
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SimplePermissions.requestPermission(Permission.ReadExternalStorage);
-    return MaterialApp(
+    return new MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter File Manager Example"),
-        ),
-        body: FutureBuilder(
-            future: getPaths(),
+          appBar: AppBar(
+            title: Text("Flutter File Manager Demo"),
+          ),
+          body: FutureBuilder(
+            future: _search(), // a previously-obtained Future<String> or null
             builder: (BuildContext context, AsyncSnapshot snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return ListView.builder(
-                  itemCount: snapshot.data?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(snapshot.data[index].path.split('/').last),
-                    );
-                  },
-                );
-              } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(child: Text("Loading"));
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Text('Press button to start.');
+                case ConnectionState.active:
+                case ConnectionState.waiting:
+                  return Text('Awaiting result...');
+                case ConnectionState.done:
+                  if (snapshot.hasError)
+                    return Text('Error: ${snapshot.error}');
+                  return snapshot.data != null
+                      ? ListView.builder(
+                          itemCount: snapshot.data.length,
+                          itemBuilder: (context, index) => Card(
+                                  child: ListTile(
+                                title: Column(children: [
+                                  Text('Size: ' +
+                                      snapshot.data[index]
+                                          .statSync()
+                                          .size
+                                          .toString()),
+                                  Text('Path: ' +
+                                      snapshot.data[index].path.toString()),
+                                  Text('Date: ' +
+                                      snapshot.data[index]
+                                          .statSync()
+                                          .modified
+                                          .toUtc()
+                                          .toString())
+                                ]),
+
+                                subtitle: Text(
+                                    "Extension: ${p.extension(snapshot.data[index].absolute.path).replaceFirst('.', '')}"), // getting extension
+                              )))
+                      : Center(
+                          child: Text("Nothing!"),
+                        );
               }
-            }),
-      ),
+              return null; // unreachable
+            },
+          )),
     );
   }
 
-  Future getPaths() async {
+  Future _search() async {
     var root = await getExternalStorageDirectory();
-    return FileManager(
-            root: root,
-            filter: SimpleFileFilter(allowedExtensions: ["png", 'apk'], hidden: false))
-        .walkFuture();
+    var fm = FileManager(
+      root: root,
+    );
+
+    List founds = await fm
+        .search(
+          // search keyword
+          "android",
+          searchFilter:
+              SimpleFileFilter(allowedExtensions: ['png'], fileOnly: true),
+          sortedBy: FileManagerSorting.Size,
+        )
+        .toList();
+
+    return founds;
   }
 }
